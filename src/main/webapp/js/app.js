96f5893c120ed9ba5c2c6a3198b52a1ce478c8d8
Sjoerd Adema <s.adema@mac.com>
2014-03-29 17:19:56 +0100

    added bootstrap dashboard template and angularjs display date and time

---
+++ src/main/webapp/js/app.js
@@ -0,0 +1,12 @@
+app = angular.module("socialcrm", []).controller("ClockController", function($scope) {
+	$scope.clock = {
+		now: new Date()
+	};
+	var updateClock = function() {
+		$scope.clock.now = new Date();
+	};
+	setInterval(function() {
+		$scope.$apply(updateClock);
+	}, 1000);
+	updateClock();
+});
